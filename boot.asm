7a8675748e9ad9871ae52f7e227de30c4c079cf1
Luiz Fonseca <eu@luiz.cc>
2018-01-22 21:23:51 -0200

    Add GDT section and read-only calcs for the initial pagin pointer

--- boot.asm
+++ boot.asm
@@ -8,27 +8,46 @@ start:
   ; p3 table
   mov eax, p3_table
   or eax, 0b11
-  mov dword [p4table + 0], eax
+  mov dword [p4_table + 0], eax
 
   ; Point the first entry of the level 3 page table to the first entry in the
   ; p2 table
   mov eax, p2_table
   or eax, 0b11
-  mov dword [p3table + 0], eax
+  mov dword [p3_table + 0], eax
 
   ; point each page table level two entry to a page / loop
   mov ecx, 0 ; counter variable
 
+  ; move page table address to cr3
+  mov eax, p4_table
+  mov cr3, eax
 
+  ; enable PAE
+  mov eax, cr4
+  or eax, 1 << 5
+  mov cr4, eax
+
+  ; set the long mode bit
+  mov ecx, 0xC0000080
+  rdmsr
+  or eax, 1 << 8
+  wrmsr
+
+  ; enable paging
+  mov eax, cr0
+  or eax, 1 << 31
+  or eax, 1 << 16
+  mov cr0, eax
 
   ; size  - place  - thing
   ; mov => 'move' thing into place
-  mov word [0xb8000], 0x1248 ; Puts letter H (0x0248) in the upper left (oxb8000)
-  mov word [0xb8002], 0x1265 ; Puts letter e (0x1265) in the upper left (0xb8002)
-  mov word [0xb8004], 0x126c ; Puts letter l (0x126c) in the upper left (0xb8004)
-  mov word [0xb8006], 0x126c ; Puts letter l (0x126c) in the upper left (oxb8006)
-  mov word [0xb8008], 0x126f ; Puts letter o (0x126f) in the upper left (oxb8008)
-  hlt ; halt
+  ;mov word [0xb8000], 0x1248 ; Puts letter H (0x0248) in the upper left (oxb8000)
+  ;mov word [0xb8002], 0x1265 ; Puts letter e (0x1265) in the upper left (0xb8002)
+  ;mov word [0xb8004], 0x126c ; Puts letter l (0x126c) in the upper left (0xb8004)
+  ;mov word [0xb8006], 0x126c ; Puts letter l (0x126c) in the upper left (oxb8006)
+  ;mov word [0xb8008], 0x126f ; Puts letter o (0x126f) in the upper left (oxb8008)
+  ;hlt ; halt
 
 .map_p2_table:
   mov eax, 0x200000  ; 2MiB
@@ -57,3 +76,25 @@ p3_table:
 
 p2_table:
   resb 4096
+
+
+
+
+
+section .rodata
+
+
+gdt64:
+  dq 0
+
+.code: equ $ - gdt64
+  dq (1<<44) | (1<<47) | (1<<41) | (1<<43) | (1<<53)
+.data: equ $ - gdt64
+  dq (1<<44) | (1<<47) | (1<<41)
+.pointer:
+  dw .pointer - gdt64 - 1
+  dq gdt64
+
+
+
+lgdt [gdt64.pointer]
